21dfc40c84f768b42267b7d87948a484edb36668
Maria <fac.san1091@gmail.com>
2026-03-26 21:12:56 -0300

    version listen corregido

--- licencias-amba-bot/src/app.js
+++ licencias-amba-bot/src/app.js
@@ -173,10 +173,6 @@ app.post("/webhook", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
-
 
 if (!process.env.VERCEL) {
   app.listen(PORT, () => {
