3aaec55d89867d03f24a5b5ce1931c03977d1a0e
Maria <fac.san1091@gmail.com>
2026-03-30 20:56:56 -0300

    nueva version mailers CORREGIDo2

--- licencias-amba-bot/src/mailer.js
+++ licencias-amba-bot/src/mailer.js
@@ -2,11 +2,16 @@ import "dotenv/config";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 15000,
+  greetingTimeout: 15000,
+  socketTimeout: 20000,
 });
 
 export async function sendLeadEmail(lead) {
